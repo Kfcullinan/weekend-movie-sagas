3f51ce28b5112611ad9550a70ca3b9377fec4df2
Keith Cullinan <kfcullinan@gmail.com>
2022-10-04 00:09:18 -0500

    completed GET in genre router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -16,4 +16,20 @@ router.get('/', (req, res) => {
   
 });
 
+// GET FOR SPECIFIC MOVIE ID 
+router.get('/:movie_id', (req, res) => {
+  const movieId = req.params.movie_id;
+  console.log('movie ID for genre', movieId);
+  const queryText = `SELECT "name" FROM "movies"
+  JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movie_id"
+  JOIN "genres" ON "genres"."id" = "movies_genres"."genre_id"
+  WHERE "movies"."id" = $1;`;
+  pool.query(queryText, [movieId]).then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('ERROR in specfic ID GET', error);
+      res.sendStatus(500);
+    });
+});
 module.exports = router;
