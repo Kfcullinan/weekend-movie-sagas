33d6b6aa4d1d2bb7e5b08c56e286a8c1cc7d8228
Keith Cullinan <kfcullinan@gmail.com>
2022-10-08 14:23:51 -0500

    completed movieDetails componentjsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,3 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+function MovieDetail() {
+    const movie = useSelector(store => store.selectedMovie);
+    return(
+        <div>
+            <h3>{movie.title}</h3>
+            <img src={movie.poster} alt={movie.title} />
+            <p>{movie.description}</p>
+        </div>
+    )
+}
+
+export default MovieDetails;
